a44c18bfa3505aad60e865db010e028e42ae7861
David Caramelo <david.caramelo83@gmail.com>
2014-12-10 21:41:16 +0100

    install Jasime and config karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp')
     , nodemon = require('gulp-nodemon');
+var karma = require('karma').server;
+
+
+
+gulp.task('test', function(done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done);
+});
 
 gulp.task('server', function () {
     nodemon({ script: 'server.js' })
---
+++ test/hello.spec.js
@@ -0,0 +1,6 @@
+describe('greeter', function () {
+
+    it('should say Hello to the World', function () {
+        expect('Hello, World!').toContain('Hello');
+    });
+});
